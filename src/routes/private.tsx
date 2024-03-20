e3918f1f515e1a4794974a2c01dc99af86e600eb
Christian <pngue006@uottawa.ca>
2024-03-20 05:37:23 -0400

    Update private.tsx

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -15,7 +15,7 @@ const PrivateRoute : FC<PrivateRouteProps> = ({ children }) => {
     const { user } = useAuth();
     if ( !user ) {
         return (
-            <Navigate to="/" replace ={true} />
+            <Navigate to="/login" replace ={true} />
           )
     }
     return children;
